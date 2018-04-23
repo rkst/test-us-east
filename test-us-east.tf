c29cf1cb6709a7457b2237b4eb91cc64dc76ad0b
Robbie T <github@rkst.it>
2018-04-23 10:00:45 -0700

    Removed variables from variables.tf, manually named

--- test-us-east.tf
+++ test-us-east.tf
@@ -4,18 +4,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_instance" "test1" {
-#   ami = "ami-1853ac65"
-#   instance_type = "t2.micro"
-#   tags {
-#     Name = "tf-test1"
-#   }
-# }
+resource "aws_instance" "test1" {
+  ami = "ami-1853ac65"
+  instance_type = "t2.micro"
+  tags {
+    Name = "${var.Name}"
+  }
+}
 
-# resource "aws_instance" "test2" {
-#   ami = "ami-1853ac65"
-#   instance_type = "t2.micro"
-#   tags {
-#     Name = "tf-test2"
-#   }
-# }
+resource "aws_instance" "test2" {
+  ami = "ami-1853ac65"
+  instance_type = "t2.micro"
+  tags {
+    Name = "tf-test2"
+  }
+}
