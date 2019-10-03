c8946b71ed03e87434e3cd537375f880369fb149
Robbie T <github@rkst.it>
2019-10-03 07:07:29 -0700

    Back to one isntance

--- test-us-east.tf
+++ test-us-east.tf
@@ -7,17 +7,8 @@ provider "aws" {
 resource "aws_instance" "test1" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t1.micro"
-  tags {
+  tags = {
     Name = "tf-test1"
     Team = "dev"
-  }
-}
-
-resource "aws_instance" "test2" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "t1.micro"
-  tags {
-    Name = "tf-test2"
-    Team = "security"
   }
 }
